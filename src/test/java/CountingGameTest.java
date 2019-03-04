32d272a618f822aa0ca837e07384983e4615896b
melody-cai <329533263@qq.com>
2019-03-05 00:28:25 +0800

    update test

--- src/test/java/CountingGameTest.java
+++ src/test/java/CountingGameTest.java
@@ -10,6 +10,11 @@ public class CountingGameTest {
     int number1 = 3;
     int number2 = 5;
     @Test
+    public void testNormalNumber() {
+        int testNumber = 7;
+        Assert.assertEquals(CountingGame.checkNum(testNumber, n -> n % number1 == 0, n -> n % number2 == 0), "7");
+    }
+    @Test
     public void testFizzNumber() {
         int testNumber = 3;
         Assert.assertEquals(CountingGame.checkNum(testNumber, n -> n % number1 == 0, n -> n % number2 == 0), "Fizz");
